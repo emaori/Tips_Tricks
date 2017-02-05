fb74f2c1c40dc5240b45bb9341081ff0ee3e0711
emaori <emanuele@origgi.com>
2017-02-05 22:01:07 +0100

    Added Get-CustomDirectory and aliases for Android Studio

--- PowerShell/DefaultProfile.ps1
+++ PowerShell/DefaultProfile.ps1
@@ -1,4 +1,3 @@
-
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 # Load posh-git module from current directory
@@ -16,9 +15,60 @@ Import-Module powerls
 # use this instead (see about_Modules for more information):
 # Import-Module posh-git
 
+# Function to Get Custom Directory path (thanks Prateek Singh - https://geekeefy.wordpress.com/2016/10/19/powershell-customize-directory-path-in-ps-prompt/)
+Function Get-CustomDirectory
+{
+    [CmdletBinding()]
+    [Alias("CDir")]
+    [OutputType([String])]
+    Param
+    (
+        [Parameter(ValueFromPipeline=$true,Position=0)]
+        $Path = $PWD.Path
+    )
+    
+    Begin
+    {
+        $desktop = "C:\Users\" + $env:USERNAME+"\Desktop"
+        
+        #Custom directories as a HashTable
+        $CustomDirectories = @{
+
+            $env:TEMP                                   ='Temp'
+            $env:HOME                                   ='HOME'
+            "C:\customers\Android\onemediahub"          ='OMH'
+        } 
+    }
+    Process
+    {
+        Foreach($Item in $Path)
+        {
+            $Match = ($CustomDirectories.GetEnumerator().name | ?{$Item -eq "$_" -or $Item -like "$_*"} |`
+            select @{n='Directory';e={$_}},@{n='Length';e={$_.length}} |sort Length -Descending |select -First 1).directory
+            If($Match)
+            {
+                [String]($Item -replace [regex]::Escape($Match),("["+$CustomDirectories[$Match]+"]"))            
+            }
+            ElseIf($pwd.Path -ne $Item)
+            {
+                $Item
+            }
+            Else
+            {
+                $pwd.Path
+            }
+        }
+    }
+    End
+    {
+    }
+}
+
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function global:prompt {
+
+    $cdelim = [ConsoleColor]::Green
 
     #$host.UI.RawUI.WindowTitle = "All Shell"
 
@@ -27,9 +77,13 @@ function global:prompt {
     # Reset color, which can be messed up by Enable-GitColors
     $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
-    Write-Host($pwd.ProviderPath) -nonewline
+    Write-Host("[" + $env:UserName + "@" + $env:COMPUTERNAME + "] ") -nonewline -f $cdelim
+    Write-Host $(Get-CustomDirectory) -ForegroundColor DarkYellow -nonewline
 
     Write-VcsStatus
+
+    # Insert new line
+    Write-Host ""
 
     $global:LASTEXITCODE = $realLASTEXITCODE
     return "> "
@@ -41,7 +95,15 @@ Pop-Location
 
 Set-Alias -Name lc -Value PowerLS -Option AllScope
 Set-Alias -Name np -Value notepad -Option AllScope
+Set-Alias -Name g -Value git -Option AllScope
+Set-Alias -Name studio -Value "C:\Program Files\Android\Android Studio\bin\studio64.exe" -Option AllScope
 
+function pwdd {
+    $pwd.Path
+}
 
+function studio-here {
+    studio $pwd.Path
+}
 
 Start-SshAgent -Quiet
